7d7ce6d1d438c851630da65cfb53fcda94d09db2
samiya zubair <94432143+samiyazbr@users.noreply.github.com>
2023-06-07 12:02:28 +0000

    gnl final

---
+++ gnl/get_next_line.c
@@ -0,0 +1,46 @@
+#include "get_next_line.h"
+
+char *get_next_line(int fd)
+{
+    int i = 0;
+    int rd = 0;
+    char ch;
+    static char *line;
+
+    line = malloc(1000000);
+    if (BUFFER_SIZE <= 0 || fd < 0)
+        return (NULL);
+    rd = read(fd, &ch, BUFFER_SIZE - BUFFER_SIZE + 1);
+    while (rd > 0)
+    {
+        line[i++] = ch;
+        if (ch == '\n')
+            break;
+        rd = read(fd, &ch, BUFFER_SIZE - BUFFER_SIZE + 1);
+    }
+    line[i] = '\0';
+    if (rd == -1 || i == 0 || (!line[i - 1] && !rd))
+    {
+        free(line);
+        return (NULL);
+    }
+    return (line);
+}
+
+int main (int ac, char **av)
+{
+	int fd;
+	char *line;
+	
+	if(ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		line = get_next_line(fd);
+		while(line)
+		{
+			printf("%s\n", line);
+			line = get_next_line(fd);
+		}
+	}
+	return (0);
+}
---
+++ gnl/get_next_line.h
@@ -0,0 +1,9 @@
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+# define BUFFER_SIZE 1
+#include <unistd.h>
+#include <stdlib.h>
+
+char *get_next_line(int fd);
+
+#endif
