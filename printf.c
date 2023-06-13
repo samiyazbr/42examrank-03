15d5d16def956562a48afe1aedf23ab59c92bfca
samiya zubair <94432143+samiyazbr@users.noreply.github.com>
2023-06-13 19:05:07 +0400

    Update printf.c

--- printf.c
+++ printf.c
@@ -1,100 +1,52 @@
-#include <stdarg.h>
 #include <unistd.h>
-#include <stdlib.h>
+#include <stdarg.h>
 
-int ft_putchar(char c)
+void	ft_putstr(char *str, int *len)
 {
-    write(1, &c, 1);
-    return (1);
+	if (!str)
+		str = "(null)";
+	while (*str)
+		*len += write(1, string++, 1);
 }
 
-int ft_putstr(char *str)
+void	ft_putnum(long long int num, int base, int *len)
 {
-    int i = 0;
+	char	*hexadecimal = "0123456789abcdef";
 
-    if (!str)
-        str = "(null)";
-    while (str[i])
-    {
-        write(1, &str[i], 1);
-        i++;
-    }
-    return (i);
+	if (num < 0)
+	{
+		num *= -1;
+		*len += write(1, "-", 1);
+	}
+	if (num >= base)
+		ft_putnum((number / base), base, len);
+	else
+		*len += write(1, &hexadecimal[number % base], 1);
 }
 
-int ft_putnbr(int num)
+int	ft_printf(const char *format, ...)
 {
-    int len = 0;
+	int len = 0;
 
-    if (num < 0)
-    {
-        len += write(1, "-", 1);
-        num *= -1;
-    }
-    else if (num <= 9 && num >= 0)
-        len += ft_putchar(num + '0');
-    else if (num > 9)
-    {
-        len += ft_putnbr(num / 10);
-        len += ft_putnbr(num % 10);
-    }
+	va_list	args;
+	va_start(args, format);
 
-    return (len);
-}
-
-int ft_printhex(unsigned int num)
-{
-    int len = 0;
-
-    if (num >= 16)
-    {
-        len += ft_printhex(num / 16);
-        len += ft_printhex(num % 16); 
-    }
-    else
-    {
-        if (num >= 10 && num < 16)
-            len += ft_putchar(num - 10 + 'a');
-        else
-            len += ft_putchar(num + '0');
-    }
-
-    return (len);
-}
-
-int	ft_printf(const char *format, ... )
-{
-    int len = 0;
-    int i = 0;
-    va_list args;
-
-    va_start(args,format);
-    while(format[i])
-    {
-        if (format[i] != '%')
-            len += ft_putchar(format[i]);
-        if (format[i] == '%' && format[i + 1])
-        {
-            i++;
-            if (format[i] == 's')
-                len += ft_putstr(va_arg(args, char *));
-            else if (format[i] == 'd')
-                len += ft_putnbr(va_arg(args, int));
-            else if (format[i] == 'x')
-                len += ft_printhex(va_arg(args, unsigned int));
-        }
-        i++;
-    }
-    va_end(args);
-    return (len);
-}
-
-#include <stdio.h>
-int main()
-{
-    int len = 0;
-	len = ft_printf("%x\n", 4253584);
-    printf("%d\n", len);
-    len = printf("%x\n", 4253584);
-    printf("%d\n", len);
+	while (*format)
+	{
+		if ((*format == '%') && ((*(format + 1) == 's') || (*(format + 1) == 'd') || (*(format + 1) == 'x')))
+		{
+			format++;
+			if (*format == 's')
+				ft_putstr(va_arg(args, char *), &len);
+			else if (*format == 'd')
+				ft_putnum((long long int)va_arg(args, int), 10, &len);
+			else if (*format == 'x')
+				ft_putnum((long long int)va_arg(args, unsigned int), 16, &len);
+		}
+		else
+			length += write(1, format, 1);
+		format++;
+	}
+	va_end(args);
+	return (len);
 }
