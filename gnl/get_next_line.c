5e6c98e26f2baaf0e19236cd45e18d5f74d9107d
samiya zubair <94432143+samiyazbr@users.noreply.github.com>
2023-06-13 18:43:12 +0400

    Update get_next_line.c

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -1,32 +1,29 @@
 #include "get_next_line.h"
 
-char *get_next_line(int fd)
+char    *get_next_line(int fd)
 {
-    int i = 0;
-    int rd = 0;
-    char ch;
-    char *line;
+        int             i;
+        int             byte;
+        char    *buffer;
+        char    character;
 
-    line = malloc(1000000);
-    if (BUFFER_SIZE <= 0 || fd < 0)
-        return (NULL);
-    rd = read(fd, &ch, BUFFER_SIZE - BUFFER_SIZE + 1);
-    while (rd > 0)
-    {
-        line[i++] = ch;
-        if (ch == '\n')
-            break;
-        rd = read(fd, &ch, BUFFER_SIZE - BUFFER_SIZE + 1);
-    }
-    line[i] = '\0';
-    if (rd == -1 || i == 0 || (!line[i - 1] && !rd))
-    {
-        free(line);
-        return (NULL);
-    }
-    return (line);
+        if ((fd < 0) || (BUFFER_SIZE <= 0))
+                return (NULL);
+        i = 0;
+        byte = read(fd, &character, 1);
+        buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+        while (byte > 0)
+        {
+                buffer[i++] = character;
+                if (character == '\n')
+                        break ;
+                byte = read(fd, &character, 1);
+        }
+        if ((byte <= 0) && (i == 0))
+                return (free(buffer), NULL);
+        buffer[i] = '\0';
+        return (buffer);
 }
-
 int main (int ac, char **av)
 {
 	int fd;
